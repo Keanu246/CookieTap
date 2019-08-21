92e1bcfaae753d8b882ee231be4a82b2e6c5721f
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-08-21 12:11:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -219,7 +219,6 @@ function renew_cookies()
 		cookie_god_enabled = 1;
 	}
 }
-}
 
 // commands and (global) variables
 
@@ -260,3 +259,5 @@ prism = new Building("Prism", 1200000000, 9000000000000);
 cookie_god = new Building("Cookie God", 450000000000, 250000000000000);
 
 setInterval(renew_cookies, 500);
+
+}
